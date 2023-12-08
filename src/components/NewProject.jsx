5a901413944a6cc2d8cdad3a0144441594c3ebb4
Irvan-Wijaya <irvan.wijaya@binus.ac.id>
2023-12-08 21:45:20 +0700

    .

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -39,9 +39,9 @@ function NewProject({ onAdd, onCancel }) {
         </li>
       </menu>
       <div>
-        <Input ref={title} label="Title" />
+        <Input type="text" ref={title} label="Title" />
         <Input ref={description} label="Description" textarea />
-        <Input ref={dueDate} label="Due Date" />
+        <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
   );
